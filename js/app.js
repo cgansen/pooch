73c73c4872d252d30fefca6de48bba0759a551de
cgansen <cgansen@gmail.com>
2010-09-29 18:55:52 -0500

    Adding localStorage for persistence across sessions

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 var Pooch = {
 	'tasks': [],
 
+	'init': function(){
+		this.tasks = [];
+	},
+	
 	'create_new_task': function(){ 
 		task = new Task( $('#task-title').val(), $('#task-due').val(), $('#task-notes').val() ); 
-		this.add_task(task);
+		this.add_task(task);		
 	},
 
 	'add_task': function(task){
 		this.tasks.push(task);
 		this.insert_task_in_list(task);
+		localStorage['pooch:tasks'] = JSON.stringify(this.tasks) // save to the local storage object
 		return this.tasks;
 	},
 
@@ -26,6 +31,21 @@ var Pooch = {
 
 	'show_notes': function(el){
 		$(el).children(".task-instance-notes").toggle();
+	},
+	
+	'load_tasks_from_JSON': function(){
+		if ( localStorage['pooch:tasks'].length - 1 ){
+			var task_list = JSON.parse(localStorage['pooch:tasks']);
+			$.each(task_list, function(idx,task){
+				if(task['title'] != undefined){
+					Pooch.add_task(new Task(task['title'], task['due'], task['notes']));
+				}
+		 	});
+		}
+	},
+
+	'reset': function(){
+		localStorage['pooch:tasks'] = JSON.stringify([]);
 	}
 
 }
@@ -35,3 +55,9 @@ function Task(title, due, notes){
 	this.due = due;
 	this.notes = notes;
 }
+
+$(document).ready(function() {
+	// load the tasks from localStorage
+	Pooch.init();
+	Pooch.load_tasks_from_JSON();
+});
